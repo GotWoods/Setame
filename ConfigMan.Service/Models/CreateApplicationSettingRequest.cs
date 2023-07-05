326de1726057380939206bd8e1ff5259c487e49d
David Woods <woodsd@protrans.com>
2023-07-05 16:30:14 -0600

    removing unused code

--- ConfigMan.Service/Models/CreateApplicationSettingRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ConfigMan.Service.Models;
-
-public class CreateApplicationSettingRequest
-{
-    public string ApplicationName{ get; set; }
-    public List<EnvironmentSetting> Settings { get; set; }
-}
--- ConfigMan.Service/Models/EnvironmentSetting.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ConfigMan.Service.Models;
-
-public class EnvironmentSetting
-{
-    public string Environment { get; set; }
-    public string Name { get; set; }
-    public string Value { get; set; }
-}
