b32a6f18b0d06cca0e16813309298bfcc960e818
dave <none@none.com>
2023-07-17 18:45:21 -0600

    simple check for rename application collisions

--- src/Service/ConfigMan.Data/Handlers/Applications/RenameApplicationHandler.cs
+++ src/Service/ConfigMan.Data/Handlers/Applications/RenameApplicationHandler.cs
@@ -8,16 +8,22 @@ public record RenameApplication(Guid ApplicationId, int ExpectedVersion, string 
 public class RenameApplicationHandler : IRequestHandler<RenameApplication>
 {
     private readonly IDocumentSession _documentSession;
+    private readonly IQuerySession _querySession;
     private readonly IUserInfo _userInfo;
 
-    public RenameApplicationHandler(IDocumentSession documentSession, IUserInfo userInfo)
+    public RenameApplicationHandler(IDocumentSession documentSession, IQuerySession querySession, IUserInfo userInfo)
     {
         _documentSession = documentSession;
+        _querySession = querySession;
         _userInfo = userInfo;
     }
 
     public async Task Handle(RenameApplication command, CancellationToken cancellationToken)
     {
+        var existing = _querySession.Query<Application>().FirstOrDefault(x => x.Name == command.NewName);
+        if (existing != null)
+            throw new ApplicationException("Conflict");
+
         await _documentSession.AppendToStreamAndSave<Application>(command.ExpectedVersion, command.ApplicationId, new ApplicationRenamed(command.NewName), _userInfo.GetCurrentUserId());
     }
 }
